4b850ac6bac64eb91dde8bb19618096bc797537c
Patrick Florek <patrick.florek@gmail.com>
2022-01-19 00:42:04 +0100

    fix(test): update snapshots

--- src/account-provider/is-complete-handler.lambda.ts
+++ src/account-provider/is-complete-handler.lambda.ts
@@ -1,6 +1,7 @@
 import { IsCompleteRequest, IsCompleteResponse } from "aws-cdk-lib/custom-resources/lib/provider-framework/types";
-import { Organizations } from "aws-sdk";
-const organizationsClient = new Organizations({ region: "us-east-1" });
+import * as AWS from "aws-sdk";
+
+let organizationsClient: AWS.Organizations;
 
 /**
  * The isComplete handler is repeatedly invoked checking CreateAccountStatus until SUCCEEDED or FAILED.
@@ -9,16 +10,20 @@ const organizationsClient = new Organizations({ region: "us-east-1" });
 export async function handler(event: IsCompleteRequest): Promise<IsCompleteResponse> {
   console.log(`Request of type ${event.RequestType} received`);
 
+  if (!organizationsClient) {
+    organizationsClient = new AWS.Organizations({ region: "us-east-1" });
+  }
+
   if (!event.PhysicalResourceId) {
     throw new Error("Missing PhysicalResourceId parameter.");
   }
 
   // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Organizations.html#describeCreateAccountStatus-property
-  const response: Organizations.DescribeCreateAccountStatusResponse = await organizationsClient
+  const response: AWS.Organizations.DescribeCreateAccountStatusResponse = await organizationsClient
     .describeCreateAccountStatus({ CreateAccountRequestId: event.PhysicalResourceId })
     .promise();
 
-  const { State, AccountId, AccountName, FailureReason } = response.CreateAccountStatus || {};
+  const { State, AccountId, AccountName, FailureReason } = response.CreateAccountStatus ?? {};
 
   if (State == "FAILED") {
     throw new Error(
