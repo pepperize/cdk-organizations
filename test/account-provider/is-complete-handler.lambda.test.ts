3bac517f8375b417b37e81774f7a45ca22d6ab5d
Patrick Florek <patrick.florek@gmail.com>
2022-01-19 12:13:21 +0100

    fix: improve custom resource tests (#28)
    Fixes #

---
+++ test/account-provider/is-complete-handler.lambda.test.ts
@@ -0,0 +1,100 @@
+import { IsCompleteHandler, IsCompleteRequest } from "aws-cdk-lib/custom-resources/lib/provider-framework/types";
+import * as SDK from "aws-sdk";
+import * as AWS from "aws-sdk-mock";
+import * as sinon from "sinon";
+
+describe("account-provider.is-complete-handler.lambda", () => {
+  jest.setTimeout(60_000);
+
+  let handler: IsCompleteHandler;
+  beforeEach(async () => {
+    //AWS.setSDKInstance(SDK);
+    AWS.setSDK(require.resolve("aws-sdk"));
+    handler = (await import("../../src/account-provider/is-complete-handler.lambda")).handler;
+    jest.resetModules();
+  });
+
+  afterEach(() => {
+    AWS.restore("Organizations");
+  });
+
+  it("Should throw an error if failed", async () => {
+    // Given
+    const mock: SDK.Organizations.DescribeCreateAccountStatusResponse = {
+      CreateAccountStatus: {
+        Id: "car-exampleaccountcreationrequestid",
+        State: "FAILED",
+        FailureReason: "Some reason",
+      },
+    };
+    const describeCreateAccountStatusFake = sinon.fake.resolves(mock);
+    AWS.mock("Organizations", "describeCreateAccountStatus", describeCreateAccountStatusFake);
+
+    const request: Partial<IsCompleteRequest> = {
+      RequestType: "Create",
+      PhysicalResourceId: "car-exampleaccountcreationrequestid",
+    };
+
+    // When
+    const response = handler(request as IsCompleteRequest);
+
+    // Then
+    await expect(async () => {
+      await response;
+    }).rejects.toThrowError("Failed Create Account undefined with Id undefined, reason: Some reason");
+    sinon.assert.called(describeCreateAccountStatusFake);
+  });
+
+  it("Should be completed when succeeded", async () => {
+    // Given
+    const mock: SDK.Organizations.DescribeCreateAccountStatusResponse = {
+      CreateAccountStatus: {
+        Id: "car-exampleaccountcreationrequestid",
+        State: "SUCCEEDED",
+        AccountId: "123456789012",
+        AccountName: "test",
+        FailureReason: undefined,
+      },
+    };
+    const describeCreateAccountStatusFake = sinon.fake.resolves(mock);
+    AWS.mock("Organizations", "describeCreateAccountStatus", describeCreateAccountStatusFake);
+
+    const request: Partial<IsCompleteRequest> = {
+      RequestType: "Create",
+      PhysicalResourceId: "car-exampleaccountcreationrequestid",
+    };
+
+    // When
+    const response = await handler(request as IsCompleteRequest);
+
+    // Then
+    sinon.assert.called(describeCreateAccountStatusFake);
+    expect(response.IsComplete).toBeTruthy();
+    expect(response.Data).toEqual({ AccountId: "123456789012", AccountName: "test" });
+  });
+
+  it("Should be not completed when in progress", async () => {
+    // Given
+    const mock: SDK.Organizations.DescribeCreateAccountStatusResponse = {
+      CreateAccountStatus: {
+        Id: "car-exampleaccountcreationrequestid",
+        State: "IN_PROGRESS",
+      },
+    };
+    const describeCreateAccountStatusFake = sinon.fake.resolves(mock);
+    AWS.mock("Organizations", "describeCreateAccountStatus", describeCreateAccountStatusFake);
+
+    const request: Partial<IsCompleteRequest> = {
+      RequestType: "Create",
+      PhysicalResourceId: "car-exampleaccountcreationrequestid",
+    };
+
+    // When
+    const response = await handler(request as IsCompleteRequest);
+
+    // Then
+    sinon.assert.called(describeCreateAccountStatusFake);
+    expect(response.IsComplete).toBeFalsy();
+    expect(response.Data).toEqual({});
+  });
+});
