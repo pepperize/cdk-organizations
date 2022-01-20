55f8f493c671ed0ce0d9a298228ae761f9d210cf
Ivan Ovdiienko <johnny.unerty@gmail.com>
2022-01-20 15:07:05 +0200

    feat: use pepperize projen-awscdk-construct

--- .projenrc.js
+++ .projenrc.js
@@ -1,12 +1,11 @@
+const { AwsCdkConstructLibrary } = require("@pepperize/projen-awscdk-construct");
 const { awscdk, javascript } = require("projen");
-const project = new awscdk.AwsCdkConstructLibrary({
+const project = new AwsCdkConstructLibrary({
   author: "Patrick Florek",
   authorAddress: "patrick.florek@gmail.com",
-  authorOrganization: true,
   license: "MIT",
   copyrightOwner: "Pepperize UG (haftungsbeschränkt)",
   cdkVersion: "2.8.0",
-  defaultReleaseBranch: "main",
   name: "@pepperize/cdk-organizations",
   description: "This project provides a CDK construct creating AWS organizations.",
   keywords: ["AWS", "CDK", "Organizations"],
@@ -14,7 +13,14 @@ const project = new awscdk.AwsCdkConstructLibrary({
 
   bundledDeps: ["aws-lambda", "aws-sdk"],
   deps: ["aws-lambda", "aws-sdk"],
-  devDeps: ["@types/aws-lambda", "cdk-nag@^2.0.0", "aws-sdk-mock", "sinon", "@types/sinon"],
+  devDeps: [
+    "@pepperize/projen-awscdk-construct@^0.0.8",
+    "@types/aws-lambda",
+    "cdk-nag@^2.0.0",
+    "aws-sdk-mock",
+    "sinon",
+    "@types/sinon",
+  ],
 
   lambdaOptions: {
     runtime: awscdk.LambdaRuntime.NODEJS_14_X,
@@ -31,8 +37,6 @@ const project = new awscdk.AwsCdkConstructLibrary({
     },
   },
 
-  dependabot: true,
-
   releaseToNpm: true,
   npmAccess: javascript.NpmAccess.PUBLIC,
   publishToNuget: {
@@ -43,18 +47,6 @@ const project = new awscdk.AwsCdkConstructLibrary({
     distName: "pepperize.cdk-organizations",
     module: "pepperize_cdk_organizations",
   },
-
-  eslint: true,
-  prettier: true,
-  prettierOptions: {
-    settings: {
-      printWidth: 120,
-    },
-  },
-
-  gitignore: [".idea"],
 });
-
-project.setScript("format", "prettier --write 'src/**/*.ts' test/**/*.ts '.projenrc.js' 'README.md' CONTRIBUTING.md");
 
 project.synth();
