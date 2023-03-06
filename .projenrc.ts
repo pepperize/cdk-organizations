9470c4ac871089ca15700629c67f4ae4172e840b
Patrick Florek <patrick.florek@gmail.com>
2023-03-06 17:40:17 +0100

    chore: update dependencies

--- .projenrc.ts
+++ .projenrc.ts
@@ -73,8 +73,6 @@ const project = new AwsCdkConstructLibrary({
   },
 });
 
-project.tasks.tryFind("docgen")?.reset("jsii-docgen -f md -o ./API");
-
 project.gitpod?.addCustomTask({
   name: "setup",
   init: "yarn install && npx projen build",
